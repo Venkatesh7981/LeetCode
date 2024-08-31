f77c9186ec0736212054294fb6d28567bb7e5f68
Venkatesh7981 <159419272+Venkatesh7981@users.noreply.github.com>
2024-08-31 19:17:27 +0530

    Time: 1 ms (85.56%), Space: 40.6 MB (70.77%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+  long start=0;
+  long end=x;
+  while(start<=end){
+    long mid=start+(end-start)/2;
+    if(mid*mid==x) return (int )mid;
+    else if(x<(mid*mid)) end=mid-1;
+    else start=mid+1;
+  }
+     return (int) end; 
+    }
+}
