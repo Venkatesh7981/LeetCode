44b30bfab9adc7a4f6be776bb093113869d8e406
Venkatesh7981 <159419272+Venkatesh7981@users.noreply.github.com>
2024-09-04 21:58:28 +0530

    Time: 44 ms (89.29%), Space: 48.3 MB (92.19%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-  return args.length; 
+  let count = 0;
+
+for (let i = 0; i < args.length; i++) {
+    count++;
+}
+
+return count;
 };
 
 /**
