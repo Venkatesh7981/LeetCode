7efacd3ce24558d5ff51c187b51c2cba089a2930
Venkatesh7981 <159419272+Venkatesh7981@users.noreply.github.com>
2024-09-22 21:54:22 +0530

    Time: 5 ms (8.87%), Space: 44.5 MB (38.65%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,17 +9,28 @@
  *     }
  * }
  */
+ import java.util.HashMap;
 public class Solution {
     public boolean hasCycle(ListNode head) {
-         ListNode slow=head;
-          ListNode fast=head;
-         while(fast !=null && fast.next!=null){
-               fast=fast.next.next;
-               slow=slow.next;
-               if(fast==slow){
-                return true;
-               }
-         }
-         return false;
+        //  ListNode slow=head;
+        //   ListNode fast=head;
+        //  while(fast !=null && fast.next!=null){
+        //        fast=fast.next.next;
+        //        slow=slow.next;
+        //        if(fast==slow){
+        //         return true;
+        //        }
+        //  }
+        //  return false;
+        HashMap<ListNode,Integer> nodemap=new HashMap<>();
+        ListNode temp=head;
+        while(temp!=null) {
+            if(nodemap.containsKey(temp)) return true;
+            else{
+                nodemap.put(temp,1);
+                temp=temp.next;
+            }
+        }
+        return false;
     }
 }
